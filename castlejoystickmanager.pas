72bcf9786a9fd507ce83c8a5bf9642b66bb6db4c
eugeneloza <zhloza@gmail.com>
2019-12-07 12:09:12 +0200

    Joystick buttons press/release Define joystick buttons

--- castlejoystickmanager.pas
+++ castlejoystickmanager.pas
@@ -4,8 +4,35 @@ interface
 
 uses
   SysUtils, Classes, Generics.Collections,
-  CastleWindow, CastleVectors, CastleJoysticks,
+  CastleWindow, CastleKeysMouse, CastleVectors, CastleJoysticks,
   CastleInternalJoystickRecord;
+
+const
+  { Joystick buttons }
+  joyA = keyPadB; {WARNING: inverting}
+  joyB = keyPadA; {WARNING: inverting}
+  joyY = keyPadX; {WARNING: inverting}
+  joyX = keyPadY; {WARNING: inverting}
+  joyBack = keyPadMinus;
+  joyStart = keyPadPlus;
+  joyLeftShoulder = keyPadL;
+  joyRightShoulder = keyPadR;
+  joyLeftTrigger = keyPadZL;
+  joyRightTrigger = keyPadZR;
+  joyLeftStick = keyPadL; {WARNING: duplicating}
+  joyRightStick = keyPadR; {WARNING: duplicating}
+  joyGuide = keyPadPlus; {WARNING: duplicating}
+  joyLeft = keyPadLeft;
+  joyRight = keyPadRight;
+  joyUp = keyPadUp;
+  joyDown = keyPadDown;
+
+  {
+  joyFakeLeft = joyLeft;
+  joyFakeRight = joyRight;
+  joyFakeUp = joyUp;
+  joyFakeDown = joyDown;
+  }
 
 type
   { Temporary: these are the routines that need to go into new TJoystick }
@@ -20,8 +47,11 @@ type
 type
   TCastleJoysticks = class
   strict private
-    JoysticksRecords: TJoystickDictionary;
-    JoysticksAdditionalData: TJoystickAdditionalDataDictionary;
+    const
+      JoystickEpsilon = 0.3;
+    var
+      JoysticksRecords: TJoystickDictionary;
+      JoysticksAdditionalData: TJoystickAdditionalDataDictionary;
 
     procedure SayJoystickEvent(const JoyName: String; const Prefix: String; const JE: TJoystickEvent; const Value: Single);
     procedure SendJoystickEvent(const Joy: TJoystick; const JE: TJoystickEvent; const Value: Single);
@@ -55,12 +85,59 @@ begin
 end;
 
 procedure TCastleJoysticks.SendJoystickEvent(const Joy: TJoystick; const JE: TJoystickEvent; const Value: Single);
+
+  function KeyToStr(const AKey: TKey): String;
+  begin
+    WriteStr(Result, AKey);
+  end;
+
+  procedure JoystickKey(const AKey: TKey; const AValue: Single);
+  var
+    UnusedStringVariable: String;
+  begin
+    if AValue > JoystickEpsilon then
+    begin
+      Container.EventPress(InputKey(TVector2.Zero, AKey, ''));
+      Container.Pressed.KeyDown(AKey, '');
+      WriteLnLog('Pressed', KeyToStr(AKey));
+    end else
+    begin
+      Container.EventRelease(InputKey(TVector2.Zero, AKey, ''));
+      Container.Pressed.KeyUp(AKey, UnusedStringVariable);
+      WriteLnLog('Released', KeyToStr(AKey));
+    end;
+  end;
+
 begin
-  {case JE of
+  case JE of
+    padA: JoystickKey(joyA, Value);
+    padB: JoystickKey(joyB, Value);
+    padY: JoystickKey(joyY, Value);
+    padX: JoystickKey(joyX, Value);
+    buttonBack: JoystickKey(joyBack, Value);
+    buttonStart: JoystickKey(joyStart, Value);
+    buttonLeftShoulder: JoystickKey(joyLeftShoulder, Value);
+    buttonRightShoulder: JoystickKey(joyRightShoulder, Value);
+    buttonLeftTrigger: JoystickKey(joyLeftTrigger, Value);
+    buttonRightTrigger: JoystickKey(joyRightTrigger, Value);
+    buttonLeftStick: JoystickKey(joyLeftStick, Value);
+    buttonRightStick: JoystickKey(joyRightStick, Value);
+    buttonGuide: JoystickKey(joyGuide, Value);
+    dpadLeft: JoystickKey(joyLeft, Value);
+    dpadRight: JoystickKey(joyRight, Value);
+    dpadUp: JoystickKey(joyUp, Value);
+    dpadDown: JoystickKey(joyDown, Value);
+
+    {{ Primary axes }
+    axisLeftX, axisLeftY,
+    axisRightX, axisRightY,
+    axisLeftXPlus, axisLeftXMinus, axisLeftYPlus, axisLeftYMinus,
+    axisRightYPlus, axisRightYMinus, {note: there are no RightXPlus/minux events in the database }}
+
     else
       ;
-  end;}
-  SayJoystickEvent(Joy.Info.Name, JoystickEventToStr(JE), JE, Value);
+  end;
+  //SayJoystickEvent(Joy.Info.Name, JoystickEventToStr(JE), JE, Value);
 end;
 
 procedure TCastleJoysticks.DoAxisMove(const Joy: TJoystick; const Axis: Byte; const Value: Single);
